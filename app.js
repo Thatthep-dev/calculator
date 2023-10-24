c9dd7e0f8ad29ccc496c527f38c69f3241db5893
Thatthep <thatthep.dev@gmail.com>
2023-10-24 23:30:12 +0700

    Basic GUI & logic complete

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const calculator = {
-  add: (x, y) => x + y,
+  add: (x, y) => parseInt(x) + parseInt(y),
   subtract: (x, y) => x - y,
   multiply: (x, y) => x * y,
   divide: (x, y) => (y !== 0 ? x / y : "Cannot divide by zero"),
@@ -10,44 +10,59 @@ function calResult(number1, operator, number2) {
     case "add":
       const addResult = calculator.add(number1, number2);
       console.log("Addition:", addResult);
-      break;
+      return addResult;
 
     case "subtract":
       const subtractResult = calculator.subtract(number1, number2);
       console.log("Subtraction:", subtractResult);
-      break;
+      return subtractResult;
 
     case "multiply":
       const multiplyResult = calculator.multiply(number1, number2);
       console.log("Multiplication:", multiplyResult);
-      break;
+      return multiplyResult;
 
     case "divide":
       const divideResult = calculator.divide(number1, number2);
       console.log("Division:", divideResult);
-      break;
+      return divideResult;
   }
 }
 
-calResult(16, "add", 4);
-
 // <-- GUI SECTION -->
 const numBtnAll = document.querySelectorAll("#numBtn");
-const oprBtnAll = document.querySelectorAll("#oprBtn");
+const oprBtnAll = document.querySelectorAll(".oprBtn");
 const resultBtn = document.querySelector("#resultBtn");
+const monitorScreen = document.querySelector("#numOnScreen");
+
+let currentInput = "";
+let operator = "";
+let result = "";
 
 numBtnAll.forEach((button) => {
   button.addEventListener("click", () => {
+    currentInput += button.textContent;
+    monitorScreen.textContent = currentInput;
     console.log(`${button.textContent} got clicked`);
   });
 });
 
 oprBtnAll.forEach((button) => {
   button.addEventListener("click", () => {
-    console.log(`${button.textContent} got clicked`);
+    result = parseInt(currentInput);
+    currentInput = "";
+    monitorScreen.textContent = currentInput;
+    operator = button.id;
+    console.log(`${operator} is Operator`);
   });
 });
 
+// Show number on monitor
+let finalResult = "";
+console.log(finalResult);
+
 resultBtn.addEventListener("click", () => {
-  console.log(`${resultBtn.textContent} got clicked`);
+  finalResult = calResult(result, operator, currentInput);
+  monitorScreen.textContent = finalResult;
+  console.log(`${finalResult} is finalResult`);
 });
